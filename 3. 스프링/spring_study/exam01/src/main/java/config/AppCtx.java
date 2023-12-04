e37710962ea5b8c7345005aab59c7e6d0e315835
3un3un <abc1987042@naver.com>
2023-12-04 17:21:35 +0900

    231204
    spring 프로젝트 생성
    세팅

---
+++ 3. 스프링/spring_study/exam01/src/main/java/config/AppCtx.java
@@ -0,0 +1,12 @@
+package config;
+
+import models.Greeter;
+import org.springframework.context.annotation.Bean;
+
+public class AppCtx {
+
+    @Bean
+    public Greeter greeter() {
+        return new Greeter();
+    }
+}
